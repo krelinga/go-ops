14ee60c6e8588fd1d263978c4f9537bfe3631f02
Andy Kreling <krelinga@gmail.com>
2025-11-01 06:43:26 +0000

    first draft of ordDefault.Ord().

--- order.go
+++ order.go
@@ -1,12 +1,15 @@
 package ops
 
-import "reflect"
+import (
+	"cmp"
+	"reflect"
+)
 
 type Ord interface {
 	Ord(Env, reflect.Value, reflect.Value) int
 }
 
-type ordTag struct {}
+type ordTag struct{}
 
 func Order[T any](env Env, a, b T) int {
 	return OrderVals(env, reflect.ValueOf(a), reflect.ValueOf(b))
@@ -39,8 +42,45 @@ func OrderVals(env Env, a, b reflect.Value) int {
 
 type ordDefault struct{}
 
+func orderLiteralCan[T cmp.Ordered](v1, v2 reflect.Value, can func(reflect.Value) bool, f func(reflect.Value) T) int {
+	if !can(v1) || !can(v2) {
+		panic(ErrInvalid)
+	}
+	return orderLiteral(v1, v2, f)
+}
+
+func orderLiteral[T cmp.Ordered](v1, v2 reflect.Value, f func(reflect.Value) T) int {
+	val1 := f(v1)
+	val2 := f(v2)
+	return cmp.Compare(val1, val2)
+}
+
 func (o ordDefault) Ord(env Env, v1, v2 reflect.Value) int {
-	return 0 // TODO
+	t := v1.Type()
+	switch t.Kind() {
+	case reflect.Struct, // TODO: implement.
+		reflect.Map,                  // TODO: maybe implement.
+		reflect.Slice, reflect.Array, // TODO: implement.
+		reflect.Chan, reflect.Func, reflect.UnsafePointer,
+		reflect.Bool, // TODO: maybe implement.
+		reflect.Uintptr,
+		reflect.Complex128, reflect.Complex64:
+		panic(ErrWrongType) // TODO: better error?
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return orderLiteralCan(v1, v2, reflect.Value.CanInt, reflect.Value.Int)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return orderLiteralCan(v1, v2, reflect.Value.CanUint, reflect.Value.Uint)
+	case reflect.Float32, reflect.Float64:
+		return orderLiteralCan(v1, v2, reflect.Value.CanFloat, reflect.Value.Float)
+	case reflect.String:
+		return orderLiteral(v1, v2, reflect.Value.String)
+	case reflect.Pointer:
+		return 0 // TODO: implement
+	case reflect.Interface:
+		return 0 // TODO: implement
+	default:
+		panic(ErrWrongType) // TODO: better error?
+	}
 }
 
 type OrdDeep struct{}
